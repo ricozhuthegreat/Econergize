a1983d77c1f389ffae4add17b090f6bbf918a89a
ricozhuthegreat <33135503+ricozhuthegreat@users.noreply.github.com>
2019-06-01 10:59:26 -0400

    Initial Commit
     - Created empty firebase project with template files for firebase login

---
+++ public/firebaseInit.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAJWkVyOlQhLk3nn-kzqYq_ZinqBgLRVUA",
+  authDomain: "econergize.firebaseapp.com",
+  databaseURL: "https://econergize.firebaseio.com",
+  projectId: "econergize",
+  storageBucket: "econergize.appspot.com",
+  messagingSenderId: "626959907168",
+  appId: "1:626959907168:web:d0e4e8ee943e86a0"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
